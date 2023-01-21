fb020c5197084dee6705e52d1fbd20783c891c97
RamiroDondero <ramiroddero@gmail.com>
2023-01-21 10:45:00 -0300

    'p'

---
+++ test/features/reserva/presentation/bloc/reservas_bloc_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
+import 'package:woki_partner/features/reserva/application/get_cant_reservas.dart';
+import 'package:woki_partner/features/reserva/application/get_list_grupo_reservas.dart';
+import 'package:woki_partner/features/reserva/application/get_list_reservas.dart';
+
+  @GenerateNiceMocks([MockSpec<GetLisTReservas>()])
+  @GenerateNiceMocks([MockSpec<GetListGrupoReservas>()])
+  @GenerateNiceMocks([MockSpec<GetCantReservas>()])
+ void main() {
+  
+}
